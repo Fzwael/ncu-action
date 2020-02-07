091ebcb16029c0a9e275a5e9d57fdafb2c2750ed
Wael Fezai <fzwael@gmail.com>
2020-02-07 10:50:52 +0100

    Make job fail if update.

--- dist/index.js
+++ dist/index.js
@@ -5755,13 +5755,17 @@ try {
     // `ncu-options` input defined in action metadata file
     const ncuOptions = core.getInput('ncu-options');
     console.log(`Running ncu with the options ${ncuOptions}!`);
-    
+
     // Run the ncu command
     // TODO : add other options
     ncu.run({
         packageManager: 'npm'
     }).then((upgraded) => {
-        console.log('dependencies to upgrade:', upgraded);
+        if (Object.keys(upgraded).length > 0) {
+            core.setFailed(`dependencies to upgrade: ${upgraded}`);
+        } else {
+            console.log('Everything is up to date. Enjoy!');
+        }
     });
 } catch (error) {
     core.setFailed(error.message);
